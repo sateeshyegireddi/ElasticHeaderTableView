e598500a1f8ead8fe855f16328dbf6bf8ee08896
Sateesh <sateesh@Sateeshs-Mac-mini.local>
2018-12-12 13:02:22 +0530

    - Project has created

---
+++ ElasticHeaderTableView/ElasticHeaderTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ElasticHeaderTableView
+//
+//  Created by Sateesh on 12/12/18.
+//  Copyright © 2018 Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
